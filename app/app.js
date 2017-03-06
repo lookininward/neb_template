f15cf3d18fb4d643ce93d5961f60fd45763c1a39
Michael <vinoth.michaelxavier@gmail.com>
2017-03-06 10:31:03 -0500

    initialize project, create route to index page

---
+++ app/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var reload = require('reload');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+app.locals.siteTitle = 'Website Template';
+
+app.use(express.static('app/public'));
+app.use(require('./routes/index'));
+
+var server = app.listen(app.get('port'), function() {
+	console.log('Listening on port ' + app.get('port'));
+});
+
+reload(server, app);
