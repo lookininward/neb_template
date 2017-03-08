2cde20a293aaa6971d5c672fdc6a9a9b84484e27
Michael <vinoth.michaelxavier@gmail.com>
2017-03-08 10:06:08 -0500

    added nodemon and reload to automate workflow

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('index.ejs', {
-		pageTitle: 'Home'
+	res.render('index', {
+	pageTitle: 'Home',
+	pageID: 'home'
 	});
 });
 
