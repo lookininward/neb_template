6d60eda6127eaac09c287cbcea627b07c54893db
Michael <vinoth.michaelxavier@gmail.com>
2017-03-08 10:35:22 -0500

    successfully passed json data to render in views

--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var reload = require('reload');
 var app = express();
 
+var dataFile = require('./data/data.json');
+
 app.set('port', process.env.PORT || 3000);
+app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.locals.siteTitle = 'Node Express | Blog Template';
+app.locals.allArticles = dataFile.articles;
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+	var data = req.app.get('appData');
+	var allArticles = [];
+
+	data.articles.forEach(function(item) {
+		allArticles = item.title;
+	});
+
 	res.render('index', {
 	pageTitle: 'Home',
-	pageID: 'home'
+	pageID: 'home',
+	articles: allArticles
 	});
 });
 
